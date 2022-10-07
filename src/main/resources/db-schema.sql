3dcf85344499eac8be16dcca14ba0d4b36da8328
Dmytro Marchenko <ace.vendigo@gmail.com>
2022-10-07 17:06:40 +0300

    Updated queries

--- src/main/resources/db-schema.sql
+++ src/main/resources/db-schema.sql
@@ -1,6 +1,7 @@
 create table dez_artist
 (
-    id               bigint not null primary key,
+    id               bigint not null
+        primary key,
     nb_fans          integer,
     name             varchar(1000),
     picture          varchar(1000),
@@ -12,41 +13,58 @@ create table dez_artist
     failed_to_load   boolean   default false
 );
 
+alter table dez_artist
+    owner to postgres;
+
 create table dez_album
 (
-    id           bigint not null primary key,
+    id           bigint not null
+        primary key,
     release_date date,
     title        varchar(255),
     artist_id    bigint
-        constraint dez_album_dez_artist_fk references dez_artist
+        constraint fkk958jrdw5yo5rpskvu08y9rf
+            references dez_artist
 );
+
+alter table dez_album
+    owner to postgres;
 
 create table dez_artist_queue
 (
-    artist_id bigint not null primary key
+    artist_id bigint not null
+        primary key
 );
+
+alter table dez_artist_queue
+    owner to postgres;
 
 create table dez_track
 (
-    id           bigint not null primary key,
-    duration     integer,
-    preview      varchar(1000),
-    release_date date,
-    title        varchar(1000),
-    album_id     bigint
-        constraint dez_track_dez_album_fk references dez_album,
-    created_date timestamp default now()
+    id            bigint not null
+        primary key,
+    duration      integer,
+    preview       varchar(1000),
+    release_date  date,
+    title         varchar(1000),
+    album_id      bigint
+        constraint fkabf8cr8xivxcbdvralsppe3r8
+            references dez_album,
+    created_date  timestamp default now(),
+    primary_track integer
 );
+
+alter table dez_track
+    owner to postgres;
 
 create table dez_contributor
 (
     track_id  bigint not null
-        constraint dez_contributor_dez_track_fk references dez_track,
+        constraint fk8rc08yolbvi6ss01jv6jxh24q
+            references dez_track,
     artist_id bigint
 );
 
-alter table dez_contributor owner to postgres;
-alter table dez_track owner to postgres;
-alter table dez_artist_queue owner to postgres;
-alter table dez_album owner to postgres;
-alter table dez_artist owner to postgres;
+alter table dez_contributor
+    owner to postgres;
+
--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -6,21 +6,24 @@ order by priority desc, nb_fans desc;
 -- Db stats
 select count(*)
 from dez_artist;
+-- 126641
 
 select count(*)
 from dez_track;
---966054
+-- 1220733
 
 -- Set low priority
 update
     dez_artist
 set priority = -80
-where nb_albums > 100 or (nb_fans < 100 and nb_albums < 3);
+where nb_albums > 100
+   or (nb_fans < 100 and nb_albums < 3);
 
 update dez_artist
 set priority = -100
-where nb_albums > 200 or length(name) > 40 or (nb_fans < 10 and nb_albums < 2);
-
+where nb_albums > 200
+   or length(name) > 40
+   or (nb_fans < 10 and nb_albums < 2);
 
 -- Move not existent collabs to artist_queue
 insert into dez_artist_queue
@@ -28,16 +31,59 @@ select distinct artist_id
 from dez_contributor dc
 where not exists(select artist_id from dez_artist da where da.id = dc.artist_id);
 
+-- Artists to export
+select id, name, picture, nb_fans as fans, priority
+from dez_artist
+order by fans desc;
 
 -- Collab songs to export
-select dt.id, dt.title, dt.duration, dt.preview, dt.release_date, string_agg(da.id::text, '|') as contributors
+select dt.id,
+                   dt.title,
+                   dt.duration,
+                   dt.preview,
+                   dt.release_date,
+                   string_agg(da.id::text, '|') as contributors
+            from dez_track dt
+                     join dez_contributor dc on dt.id = dc.track_id
+                     join dez_artist da on dc.artist_id = da.id
+            where dt.primary_track = 1
+            group by dt.id, dt.title, dt.duration, dt.preview, dt.release_date
+            having count(dc.artist_id) > 1;
+
+-- Smaller db (To fit into Aurora free tier)
+-- Artists
+select id, name, picture, nb_fans as fans, priority
+from dez_artist
+where priority > -90
+order by priority desc, fans desc;
+
+-- Tracks
+select dt.id,
+       dt.title,
+       dt.duration,
+       dt.preview,
+       dt.release_date,
+       string_agg(da.id::text, '|') as contributors
 from dez_track dt
          join dez_contributor dc on dt.id = dc.track_id
          join dez_artist da on dc.artist_id = da.id
+where dt.primary_track = 1
 group by dt.id, dt.title, dt.duration, dt.preview, dt.release_date
 having count(dc.artist_id) > 1;
 
--- Artists to export
-select id, name, picture, nb_fans as fans from dez_artist order by fans desc;
+-- Set primary tracks
+update dez_track set primary_track = 1 where id in (
+select distinct trackId  from (
+select dt.id as trackId, a1.id, a2.id, rank() over (partition by a1.id, a2.id order by dt.id desc) as trackRank
+from dez_artist a1
+         join dez_contributor dc1 on a1.id = dc1.artist_id
+         join dez_track dt on dc1.track_id = dt.id
+         join dez_contributor dc2 on dc2.track_id = dt.id
+         join dez_artist a2 on dc2.artist_id = a2.id
+where a1.id > a2.id and a1.priority > -90 and a2.priority > -90
+order by a1.id, a2.id) as ranks
+where trackRank = 1);
+
+
 
 
